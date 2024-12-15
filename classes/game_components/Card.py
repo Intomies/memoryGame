ba65a59d5bc7b3ced626e32691ab5e6cbcfd66e9
Valtteri Nummi <valtteri.nummi@gmail.com>
2024-12-15 21:03:16 +0200

    feat: added sound for card turn

--- classes/game_components/Card.py
+++ classes/game_components/Card.py
@@ -1,5 +1,7 @@
 from pygame import Rect, Surface
+from pygame.mixer import Sound
 
+from utils.settings import Paths, Sounds
 
 class Card:
     def __init__(
@@ -15,6 +17,8 @@ class Card:
         self.found_pair: bool = False
         self.selected_img: Surface = self.back_img if not self.found_pair else self.front_img
         self.rect: Rect = self.selected_img.get_rect()
+        self.sound = Sound(Paths.sound(Sounds.turn))
         
     def turn(self):
+        self.sound.play()
         self.selected_img = self.front_img if self.selected_img != self.front_img else self.back_img
