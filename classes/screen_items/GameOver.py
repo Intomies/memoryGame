4385a5ee47713345d5ab9874f9e8b70f77a80c94
Valtteri Nummi <valtteri.nummi@gmail.com>
2025-04-12 23:11:55 +0300

    fix: typo

--- classes/screen_items/GameOver.py
+++ classes/screen_items/GameOver.py
@@ -125,7 +125,7 @@ class GameOver:
         highest_score = max(self.players, key=lambda player: player.score) 
         lowest_score = min(self.players, key=lambda player: player.score) 
 
-        if len(self.players == 1):
+        if len(self.players) == 1:
             return f'Game ended on turn {self.turns}!'
 
         if highest_score == lowest_score:
